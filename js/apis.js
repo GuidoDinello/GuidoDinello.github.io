2ec942287667987e396f3d9ee82e0838262bb1d8
Guido Dinello <guido.dinello@gmail.com>
2022-07-26 17:09:27 -0300

    extras page + api.js + extras.css + styles.css modified

---
+++ js/apis.js
@@ -0,0 +1,51 @@
+
+const urls = [
+    "https://rapidapi.com/montanaflynn/api/fifa-world-cup/details",
+    "https://api.coindesk.com/v1/bpi/currentprice.json",
+    "https://www.boredapi.com/api/activity",
+    "https://dog.ceo/api/breeds/image/random",
+    "https://official-joke-api.appspot.com/random_joke",
+    "https://rapidapi.com/ronreiter/api/meme-generator",
+    "http://developer.simsimi.com/",
+    "https://boggio-analytics.com/fp-api/",
+    "https://covid19-api.com/"
+]
+
+function getJSONData(url) {
+    return fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw Error(response.statusText);
+        }
+    })
+    .then(function(response) {
+        result.status = 'ok';
+        result.data = response;
+        return result;
+    })
+    .catch(function(err) {
+        result.status = 'error';
+        result.data = error;
+        return result;
+    })
+}
+
+function requestBtnAPI(btn) {
+    return ;
+}
+
+
+function main(){
+    let container = document.getElementById("apis-container");
+    let info = recieveInfo();
+    container.innerHTML = `
+        <article class="FIFA-WC">
+            <p>Follow the fifa worldcup</p> 
+        </article>
+    `
+}
+
+main();
+
